2b01d731780a454bdee8e2a1480e4fdd3fb81912
MarissaFosse <59444295+MarissaFosse@users.noreply.github.com>
2022-11-10 15:27:59 -0500

    Update streamlit_app.py
    changed streamlit.list to streamlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.list('Omega 3 & Blueberry Oatmeal','Kale, Spinach & Rocket Smoothie','Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal','Kale, Spinach & Rocket Smoothie','Hard-Boiled Free-Range Egg')
