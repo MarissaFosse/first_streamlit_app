cedf38c83f4b1e8c8fa5a0ce071ad878096ce6e1
MarissaFosse <59444295+MarissaFosse@users.noreply.github.com>
2022-12-03 00:33:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
 #don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
   
 #New section to display 2nd textbox
 def insert_row_snowflake(new_fruit):
